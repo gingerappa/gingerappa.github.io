1d4d73b549a43eea63f367077f41d3b68d4292df
gingerappa <t.swinkels@sintlucasedu.nl>
2022-10-06 14:36:21 +0200

    trying to fix json

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 import { Vector3 } from 'https://unpkg.com/three@0.127.0/build/three.module.js';
-import * as data from './json/html.json';
+import * as data from './html.json';
 //import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 //import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 
 //variables
-let log = [false];
+let running = false;
 const body = document.getElementById("body");
 
 //textures
@@ -16,6 +16,8 @@ const earthTexture = new THREE.TextureLoader().load('img/earthTX.jpg')
 const earthNormal = new THREE.TextureLoader().load('img/earthNM.jpg')
 const marsTexture = new THREE.TextureLoader().load('img/marsTX.jpg')
 const marsNormal = new THREE.TextureLoader().load('img/marsNM.jpg')
+const jupiterTexture = new THREE.TextureLoader().load('img/jupiterTX.jpg')
+const jupiterNormal = new THREE.TextureLoader().load('img/jupiterNM.jfif')
 
 
 //setting everything up
@@ -36,12 +38,18 @@ const earth = new THREE.Mesh(
 const mars = new THREE.Mesh(
   new THREE.SphereGeometry(14, 100, 100),
   new THREE.MeshStandardMaterial({map: marsTexture, wireframe: false, normalMap: marsNormal}))
+const jupiter = new THREE.Mesh(
+  new THREE.SphereGeometry(14, 100, 100),
+  new THREE.MeshStandardMaterial({map: jupiterTexture, wireframe: false}))
 earth.rotation.set(0.3, 2, 0.1)
 mars.position.set(48, -22, 40)
 mars.rotation.set(0.3, 2, 0.1)
+jupiter.position.set(96, -44, 80)
+jupiter.rotation.set(0.3, 2, 0.1)
 
-var PageCord = [[10, 0, 27, "homePage"], [58, -22, 67, "Year1Home"]]
-scene.add(earth,mars);
+
+var PageCord = [[10, 0, 27, "homePage"], [58, -22, 67, "Year1Home"], [106, -44, 107, "test"]]
+scene.add(earth,mars,jupiter);
 
 
 // adding light 0xFFE87C
@@ -66,6 +74,7 @@ function run(){
 
   earth.rotation.y += 0.0005
   mars.rotation.y += 0.001
+  jupiter.rotation.y += 0.0002
 
   renderer.render(scene, camera);
 }
@@ -84,53 +93,45 @@ function getDifference(a, b){
   }
 }
 function loadPage(pageName){
-  console.log(data.html[pageName])
   body.innerHTML = data.html[pageName]
   document.getElementById("left").onclick = function() {switchPlanet("left")};
   document.getElementById("right").onclick = function() {switchPlanet("right")};
 }
 
 function move(object, newPosition, step){
-  if(!log[0]){
-    log[0] = true
-    var positionObject = object.position
-    var steps = [getDifference(positionObject.x, newPosition[0])/step, getDifference(positionObject.y, newPosition[1])/step, getDifference(positionObject.z, newPosition[2])/step]
-    var fovStep = 120/step
-    var count = 0
-    function test(){
-      if(count <= step){
-        var positionObject = object.position
-        object.position.set(positionObject.x + steps[0], positionObject.y + steps[1], positionObject.z + steps[2])
-        count ++;
-        requestAnimationFrame(test)
-      }
-      else{
-        log[0] = false
-        camera.updateProjectionMatrix();
-        if(log.length != 1){
-          move(log[1][0], log[1][1], log[1][2])
-          log.splice(1, 1)
-        }
-      }
+  var positionObject = object.position
+  var steps = [getDifference(positionObject.x, newPosition[0])/step, getDifference(positionObject.y, newPosition[1])/step, getDifference(positionObject.z, newPosition[2])/step]
+  var fovStep = 120/step
+  var count = 0
+  function test(){
+    if(count <= step){
+      var positionObject = object.position
+      object.position.set(positionObject.x + steps[0], positionObject.y + steps[1], positionObject.z + steps[2])
+      count ++;
+      requestAnimationFrame(test)
     }
-    test()
+    else{
+      loadPage(PageCord[0][3])
+      running = false
+    }
   }
-  else{
-    log.push([object, newPosition, step])
-  }
+  test()
 }
 
 function switchPlanet(directions){
-  if (directions == "left"){
-    PageCord.unshift(PageCord[PageCord.length - 1])
-    PageCord.splice(PageCord.length - 1, 1)
+  if(!running){
+    running = true
+    if (directions == "left"){
+      PageCord.unshift(PageCord[PageCord.length - 1])
+      PageCord.splice(PageCord.length - 1, 1)
+    }
+    else{
+      PageCord.push(PageCord[0])
+      PageCord.splice(0, 1)
+    }
+    move(camera, PageCord[0], 120)
+    
   }
-  else{
-    PageCord.push(PageCord[0])
-    PageCord.splice(0, 1)
-  }
-  loadPage(PageCord[0][PageCord.length+1])
-  move(camera, PageCord[0], 120)
 }
 
 window.addEventListener("keydown", function (event) {
